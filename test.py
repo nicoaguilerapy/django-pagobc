2966b4086f3873bed1dd200eb1b5077fe484c45d
Nicolas Notebook <uverodevpy@gmail.com>
2021-12-13 04:09:06 -0300

    COSO

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-token_compra = 'bdb0add7b6367f6ff9a8d3a310f933ba343bf71b6e23dc3af9d9bf14a93743b2'
-token_privado = 'b3a23b044e9de4f3740d2a4d552beea4'
-text = "{}{}".format(token_privado, token_compra)
-print(text)
+from core.models import Payment
+import time
+
+# starting time
+start = time.time()
+
+for i in range(0, 100):
+    pay = Payment.objects.create(concept = 'test', mount=i)
+    pay.concept = 'hola'
+    pay.save()
+    pay.delete()
+
+end = time.time()
+print(f"Ejecutado en {end - start}")
